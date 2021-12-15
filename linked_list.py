a6e3879094f9a21e560d14d32912e285f25f1aa1
Hemanth Kumar <digitalhemanth@gmail.com>
2021-12-15 23:13:09 +0530

    Create linked_list.py

---
+++ linked_list.py
@@ -0,0 +1,80 @@
+from types import new_class
+
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+class linked_list:
+    def __init__(self):
+        self.head = None
+                    
+    def inserssion(self,data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+    
+    def ins_end(self,data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            n = self.head
+            while n.next is not None:
+                n = n.next
+            n.next=new_node
+    
+    def inset_any(self,data,x):
+        print(x)
+        n = self.head
+        while n is not None:
+            if x == n.data:
+               break
+            n = n.next
+        if n is None:
+            print("The element is not found in the Linked list")
+        else:    
+            new_node = Node(data)       
+            new_node.next = n.next
+            n.next = new_node
+            
+    def add_before(self,data,x):
+        if self.head.data == x:
+            new_node = Node(data)
+            new_node.next = self.head
+            self.head = new_node
+            return
+        n = self.head
+        while n is not None:
+            if x == n.next.data:   
+                break
+            n = n.next
+        if n is None:
+            print(" Sorry the Element is Not found")
+        else:
+            new_node = Node(data)
+            new_node.next = n.next
+            n.next = new_node     
+             
+    def show(self):
+        n = self.head
+        if n is None:
+            print("Liked list is Empty : ")
+        while n is not None:
+            print(n.data, end='-> ')
+            n = n.next
+    
+#obj = Node('A')
+
+cobj = linked_list()
+cobj.inserssion('B')
+cobj.inserssion('A')
+cobj.ins_end('H')
+cobj.inset_any('S','V')
+cobj.inset_any('S','B')
+cobj.add_before('L','B')
+cobj.show()
+#print(obj.data)
+#print(obj.next)
+        
