94d6e0f6cdfebe9b30ccc86ade2e2368ac3a296b
Hemanth Kumar <digitalhemanth@gmail.com>
2021-12-20 23:38:44 +0530

    Create Anagram.py

---
+++ Practice-2/Anagram.py
@@ -0,0 +1,23 @@
+'''
+str1 = input("Enter Str 1")
+str2 = input("Enter Str 2")
+
+if sorted(str1)==sorted(str2):
+    print("Stings are Anagram!")
+'''
+
+strings = []
+inputs = input("Please enter list of strings: \n").lower()
+strings = inputs.split()
+print(strings)
+
+anagramse={}
+
+for word in strings:
+    sorted_str = str(sorted(word))
+    if sorted_str in anagramse:
+        anagramse[sorted_str].append(word)
+    else:
+        anagramse[sorted_str] = [word]
+
+print(list(anagramse.values()))
