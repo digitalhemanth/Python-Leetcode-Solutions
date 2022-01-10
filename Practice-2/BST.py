6cbcf7486a562c1b27724df9eea5f9adccb82df9
Hemanth Kumar <digitalhemanth@gmail.com>
2022-01-10 23:05:55 +0530

    Create BST.py

---
+++ Practice-2/BST.py
@@ -0,0 +1,30 @@
+class BST:
+    def __init__(self,key):
+        self.key = key
+        self.lnode = None
+        self.rnode = None
+        
+    def insert(self,data):
+        if self.key is None:
+            self.key = BST(data)
+            return
+        if self.key > data:
+            if self.lnode:
+               self.lnode.insert(data)
+            else:
+                self.lnode = BST(data)
+        else:
+            if self.rnode:
+                self.rnode.insert(data)
+            else:
+                self.rnode = BST(data)
+                
+root = BST(2)
+print(root.key)
+
+tree = [6,8,3,4,1,0,7,9]
+
+for i in tree:
+    root.insert(i)
+
+print(root)
