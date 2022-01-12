3275bbdbf58c899ed44ad06b9d2076c1e5cc431c
Hemanth Kumar <digitalhemanth@gmail.com>
2022-01-12 23:02:49 +0530

    Create exceptionhandling.py

---
+++ Practice-2/exceptionhandling.py
@@ -0,0 +1,20 @@
+'''
+Python Errors and Built-in Exceptions
+These errors can be broadly classified into two classes:
+1.	Syntax errors
+2.	Logical errors (Exceptions)
+
+Python Syntax Errors
+Error caused by not following the proper structure (syntax) of the language is called syntax error or parsing error.
+
+Python Logical Errors (Exceptions)
+Errors that occur at runtime (after passing the syntax test) are called exceptions or logical errors.
+'''
+
+#KeyboardInterrupt:	Raised when the user presses Ctrl+c, Ctrl+z or Delete
+#ImportError : Raised when an imported module does not exist
+#MemoryError	Raised when a program runs out of memory
+#NameError	Raised when a variable does not exist
+#OverflowError	Raised when the result of a numeric calculation is too large
+#ZeroDivisionError	Raised when the second operator in a division is zero
+#TypeError	Raised when two different types are combined
