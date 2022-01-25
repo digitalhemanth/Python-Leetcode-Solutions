3148ea83d2bb6da5133cee292b83fe744c774579
Hemanth Kumar <digitalhemanth@gmail.com>
2022-01-25 23:55:50 +0530

    Update Number_code.py

--- Sample_code_test/Number_code.py
+++ Sample_code_test/Number_code.py
@@ -130,4 +130,23 @@ for i in str(num):
     sum = int(i)+sum
     
 print(sum)
+
+
+# Python program to swap two variables
+
+x = 5
+y = 10
+
+# To take inputs from the user
+#x = input('Enter value of x: ')
+#y = input('Enter value of y: ')
+
+# create a temporary variable and swap the values
+temp = x
+x = y
+y = temp
+
+print('The value of x after swapping: {}'.format(x))
+print('The value of y after swapping: {}'.format(y))
+
     
