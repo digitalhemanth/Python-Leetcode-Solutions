530956c2733b770524ba068d3913b43effbfa48c
Hemanth Kumar <digitalhemanth@gmail.com>
2022-01-26 23:43:16 +0530

    Update python_string_programming.py

--- Sample_code_test/python_string_programming.py
+++ Sample_code_test/python_string_programming.py
@@ -25,3 +25,21 @@ print(res)
 name = 'hemanth'
 res = name.count('h')
 print(res)
+
+
+# Find the missing number in a given list
+arr = [1, 2, 3, 4, 6, 7, 8, 9, 10]
+# get the array's length
+n = len(arr)
+
+# actual size is `n+1` since a number is missing from the list
+m = n + 1
+
+# get a sum of integers between 1 and `n+1`
+total = m * (m + 1) // 2
+
+# the missing number is the difference between the expected sum and
+# the actual sum of integers in the list
+res = total - sum(arr)
+
+print(res)
