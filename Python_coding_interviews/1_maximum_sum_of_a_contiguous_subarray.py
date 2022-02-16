42737d4e52e22ed1e185cf2779dc06fcb92b5d24
Hemanth Kumar <digitalhemanth@gmail.com>
2022-02-16 23:25:09 +0530

    Update 1_maximum_sum_of_a_contiguous_subarray.py

--- Python_coding_interviews/1_maximum_sum_of_a_contiguous_subarray.py
+++ Python_coding_interviews/1_maximum_sum_of_a_contiguous_subarray.py
@@ -1,3 +1,17 @@
 # Function to find the maximum sum of a contiguous subarray
 # in a given integer array
 
+def solution(array):
+    
+    max_of_sum = 0
+    max_ending = 0
+    
+    for i in array:
+        max_ending = max_ending + i
+        max_ending = max(max_ending,0)
+        max_of_sum = max(max_of_sum,max_ending)
+    
+    return max_of_sum
+
+array = [-2, 100, -3, 4, -1, 2, 1, -5, 4]
+print(solution(array))
