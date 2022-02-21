01e87f99d2b87051cfb6af824d18eaeb47e7b6ed
Hemanth Kumar <digitalhemanth@gmail.com>
2022-02-21 23:56:29 +0530

    Create Duplicate_string.py

---
+++ Sample_code_test/Duplicate_string.py
@@ -0,0 +1,24 @@
+duplicate = []
+name = "hemanth kumar"
+
+for i in name:
+    if name.count(i) > 1:
+       if i not in duplicate:
+          duplicate.append(i)
+
+print(duplicate)
+
+
+
+## initializing string
+string = "tutorialspoint"
+## initializing a list to append all the duplicate characters
+duplicates = []
+for char in string:
+   ## checking whether the character have a duplicate or not
+   ## str.count(char) returns the frequency of a char in the str
+   if string.count(char) > 1:
+   ## appending to the list if it's already not present
+   if char not in duplicates:
+   duplicates.append(char)
+print(*duplicates)
