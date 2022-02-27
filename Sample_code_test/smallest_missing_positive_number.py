ad88f4c65332a478d008891a1d49458577e78f05
Hemanth Kumar <digitalhemanth@gmail.com>
2022-02-27 22:56:46 +0530

    Update smallest_missing_positive_number.py

--- Sample_code_test/smallest_missing_positive_number.py
+++ Sample_code_test/smallest_missing_positive_number.py
@@ -1,3 +1,19 @@
-nums = [1, 4, 2, -1, 6, 5]
-distinct = {*nums}
-print(distinct) 
+# Function to find the smallest missing positive number from an unsorted array
+def findSmallestMissing(nums):
+ 
+    # initialize the set from array elements
+    distinct = {*nums}
+ 
+    # return first smallest missing positive number from the set
+    index = 1
+    while True:
+        if index not in distinct:
+            return index
+        index += 1
+ 
+ 
+if __name__ == '__main__':
+ 
+    nums = [1, 4, 2, -1, 3]
+    print('The smallest missing positive number from the array is',
+        findSmallestMissing(nums))
