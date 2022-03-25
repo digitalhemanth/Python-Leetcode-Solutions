4dc9995dfa7badac54d97db5521b6a4cc7af6d74
Hemanth Kumar <digitalhemanth@gmail.com>
2022-03-25 23:32:25 +0530

    Update Interviews.py

--- Interviews.py
+++ Interviews.py
@@ -2,6 +2,8 @@ from re import M
 
 
 My_list = [6,1,3,1,9,6,0,1,7,7]
+
+'''
 li = []
 
 for i in My_list:
@@ -9,3 +11,9 @@ for i in My_list:
         li.append(i)
     else:
         print(i,end=' ')
+'''
+
+dups = { x for x in My_list if My_list.count(x)>1 }
+
+print(dups)
+    
