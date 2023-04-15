c7a37a6e00639c4ebcdde4320bb714ef854dd45c
Hemanth Kumar <digitalhemanth@gmail.com>
2023-04-16 00:06:19 +0530

    Update list_dict_comprahentsion.py

--- 2023/list_dict_comprahentsion.py
+++ 2023/list_dict_comprahentsion.py
@@ -1,4 +1,11 @@
 lists = [4,2,8]
 
-duble = [num*2 for num in list]
+duble = [num*2 for num in lists]
 print(duble)
+
+
+names = ['hemanth','Hem']
+ages = [27,28]
+
+candiates = {name:age for name in names for age in ages}
+print(candiates)
