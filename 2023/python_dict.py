2eac17d9606199e7543a6e3a7bcba25df54a4852
Hemanth Kumar <digitalhemanth@gmail.com>
2023-04-20 00:30:59 +0530

    Update python_dict.py

--- 2023/python_dict.py
+++ 2023/python_dict.py
@@ -1,4 +1,9 @@
-
-my_dict = {}
-
-
+class Myval:
+    def __init__(self,id, name, age, location):
+        self.id = id
+        self.name = name
+        self.age = age
+        self.location = location
+        
+    def add_vall
+        
