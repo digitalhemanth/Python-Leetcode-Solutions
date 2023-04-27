e0faaa1fbcf20f4055e8b557f9330819efa2d56e
Hemanth Kumar <digitalhemanth@gmail.com>
2023-04-27 23:52:20 +0530

    Update python_sets.py

--- 2023/python_sets.py
+++ 2023/python_sets.py
@@ -1,1 +1,10 @@
 # In Python, a set is an unordered collection of unique elements. Sets are defined using curly braces {} or the set() function.
+
+
+note = {2,2,3}
+
+note.add(5)
+
+note.remove(2)
+
+print(note)
