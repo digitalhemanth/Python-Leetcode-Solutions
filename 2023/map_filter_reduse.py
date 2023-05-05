b23c598429e160fe675ade6b31745d3a8c0f318c
Hemanth Kumar <digitalhemanth@gmail.com>
2023-05-05 09:22:43 +0530

    Update map_filter_reduse.py

--- 2023/map_filter_reduse.py
+++ 2023/map_filter_reduse.py
@@ -8,3 +8,15 @@
 # reduce()
 # The reduce() function takes a function and an iterable as arguments, and applies the function to the first two elements of the iterable, 
 # then to the result and the next element, and so on, until a single value is returned.
+
+
+from functools import reduce
+
+def multiply(x, y):
+    result = x * y
+    print(f"Multiplying {x} and {y} to get {result}")
+    return result
+
+numbers = [1, 2, 3, 4, 5]
+product = reduce(multiply, numbers)
+print("Final product:", product)
