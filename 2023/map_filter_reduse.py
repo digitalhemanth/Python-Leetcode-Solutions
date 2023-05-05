fedb86adc0b1fa5d3a52a158d216e7b06fdc7c16
Hemanth Kumar <digitalhemanth@gmail.com>
2023-05-05 09:21:13 +0530

    Create map_filter_reduse.py

---
+++ 2023/map_filter_reduse.py
@@ -0,0 +1,10 @@
+#map() : The map() function takes a function and an iterable as arguments, and applies the function to each element of the iterable,
+# returning an iterator with the results.
+
+# filter()
+# The filter() function takes a function and an iterable as arguments, and returns an iterator with the elements of the iterable for which the function returns True.
+
+
+# reduce()
+# The reduce() function takes a function and an iterable as arguments, and applies the function to the first two elements of the iterable, 
+# then to the result and the next element, and so on, until a single value is returned.
