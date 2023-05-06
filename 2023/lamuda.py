2ce8a5e4e7e7a7b18cdc63b49e0bbda719ac838f
Hemanth Kumar <digitalhemanth@gmail.com>
2023-05-06 23:52:20 +0530

    Update lamuda.py

--- 2023/lamuda.py
+++ 2023/lamuda.py
@@ -1,1 +1,8 @@
 # ambda function in Python, often called a lambda expression or anonymous function. In Python, a lambda function is a way to create small, anonymous functions without using the def keyword.
+
+
+# lambda arguments: expression
+#
+
+a = lambda x,y : x/y
+print(a(8,2))
