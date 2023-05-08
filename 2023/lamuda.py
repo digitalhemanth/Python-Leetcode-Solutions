67e868a3f0ed3e2c95b96b4cafd049032649c392
Hemanth Kumar <digitalhemanth@gmail.com>
2023-05-08 23:52:12 +0530

    Update lamuda.py

--- 2023/lamuda.py
+++ 2023/lamuda.py
@@ -6,3 +6,10 @@
 
 a = lambda x,y : x/y
 print(a(8,2))
+
+
+# In Python, args and kwargs are commonly used to handle variable-length arguments in functions.
+
+# args stands for "arguments" and is used to pass a variable number of non-keyword arguments to a function. It allows you to pass an arbitrary number of positional arguments, which are then accessible as a tuple within the function.
+
+
