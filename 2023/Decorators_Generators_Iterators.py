1f180a2b11e78e879eae3107bd0e21a7bf099e0e
Hemanth Kumar <digitalhemanth@gmail.com>
2023-05-09 23:40:08 +0530

    Update Decorators_Generators_Iterators.py

--- 2023/Decorators_Generators_Iterators.py
+++ 2023/Decorators_Generators_Iterators.py
@@ -1,16 +1,18 @@
-# def main_code(macros):
-#     def say_hai():
-#         print("just called")
-#         macros()
-#         print("helow ms hemanth")
-#     return say_hai
+def main_code(macros):
+    def say_hai():
+        print("just called")
+        macros()
+        print("helow ms hemanth")
+    return say_hai
 
-# @main_code
-# def helow():
-#     print("hey im from aditinal function")
+@main_code
+def helow():
+    print("hey im from aditinal function")
 
-# helow()
+helow()
 
+
+#Itarators 
 
 class MyIterator:
     def __init__(self, data):
