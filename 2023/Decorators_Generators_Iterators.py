09302345d0c87ceb40ec082c63ecd8b3c9b0bf78
Hemanth Kumar <digitalhemanth@gmail.com>
2023-05-09 23:39:34 +0530

    Update Decorators_Generators_Iterators.py

--- 2023/Decorators_Generators_Iterators.py
+++ 2023/Decorators_Generators_Iterators.py
@@ -1,12 +1,34 @@
-def main_code(macros):
-    def say_hai():
-        print("just called")
-        macros()
-        print("helow ms hemanth")
-    return say_hai
+# def main_code(macros):
+#     def say_hai():
+#         print("just called")
+#         macros()
+#         print("helow ms hemanth")
+#     return say_hai
 
-@main_code
-def helow():
-    print("hey im from aditinal function")
+# @main_code
+# def helow():
+#     print("hey im from aditinal function")
 
-helow()
+# helow()
+
+
+class MyIterator:
+    def __init__(self, data):
+        self.data = data
+        self.index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index >= len(self.data):
+            raise StopIteration
+        item = self.data[self.index]
+        self.index += 1
+        return item
+
+# Usage:
+my_list = [1, 2, 3]
+my_iterator = MyIterator(my_list)
+for item in my_iterator:
+    print(item)
