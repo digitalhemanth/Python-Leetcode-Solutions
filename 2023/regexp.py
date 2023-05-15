d699f794483db3b6606e1c234724959b8eb0eaac
Hemanth Kumar <digitalhemanth@gmail.com>
2023-05-15 23:57:02 +0530

    Update regexp.py

--- 2023/regexp.py
+++ 2023/regexp.py
@@ -4,7 +4,7 @@ import re
 pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b'
 
 # Create a string to search within
-text = 'Contact us at email@example.com or support@example.com'
+text = 'Contact us at email@example.com or support@example.com or my_com any'
 
 # Search for matches using the pattern
 matches = re.findall(pattern, text)
