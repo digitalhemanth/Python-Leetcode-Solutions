5e2036381e41cec1541bcccd2779ae3d71ca4a24
Hemanth Kumar <digitalhemanth@gmail.com>
2023-05-15 23:55:09 +0530

    Update regexp.py

---
+++ 2023/regexp.py
@@ -0,0 +1,14 @@
+import re
+
+# Define a pattern
+pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b'
+
+# Create a string to search within
+text = 'Contact us at email@example.com or support@example.com'
+
+# Search for matches using the pattern
+matches = re.findall(pattern, text)
+
+# Print the matches
+for match in matches:
+    print(match)
