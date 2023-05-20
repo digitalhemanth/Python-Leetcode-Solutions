597ee2bde082e5e5300b6419d4a91d6f3bac5561
Hemanth Kumar <digitalhemanth@gmail.com>
2023-05-20 23:47:21 +0530

    Update python_folders_files.py

---
+++ 2023/Python file Operations/Files/python_folders_files.py
@@ -0,0 +1,22 @@
+import os
+
+def count_and_list_folders(path):
+    folder_count = 0
+    folder_list = []
+
+    for root, dirs, files in os.walk(path):
+        for dir in dirs:
+            folder_count += 1
+            folder_list.append(os.path.join(root, dir))
+
+    return folder_count, folder_list
+
+# Provide the path to the XYZ folder
+folder_path = '/path/to/XYZ'
+
+count, folders = count_and_list_folders(folder_path)
+
+print("Number of subfolders:", count)
+print("List of subfolders:")
+for folder in folders:
+    print(folder)
