5a9e68a576464020944f1e559873320a024cae5e
Hemanth Kumar <digitalhemanth@gmail.com>
2023-06-10 09:35:59 +0530

    Update binary_search.py

--- 2023_leetcode_python/binary_search.py
+++ 2023_leetcode_python/binary_search.py
@@ -2,23 +2,29 @@ def BibarySerch(Arryas, target):
     left = 0
     right = len(Arryas)-1
     
+    loop = 1
+    
    
     while left <= right :
         middle = (left + right) //2
         mid_ele =  Arryas[middle]
-        # print(mid_ele)
+   
+        print(f'{mid_ele} at loop number {loop}')
+        loop += 1
         
         if mid_ele == target:
             return middle
         elif mid_ele > target:
             right = middle - 1
+           
         else :
             left = middle + 1
+            
 
     return "element not found"
     
 
-print(BibarySerch([2,4,6,8,12,14], 5))
+print(BibarySerch([2,4,6,8,12,14], 8))
 
 
 
