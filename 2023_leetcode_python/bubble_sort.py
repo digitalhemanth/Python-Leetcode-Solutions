1d5f7caf9328f600c05f0dc62b3454a85acfe09b
Hemanth Kumar <digitalhemanth@gmail.com>
2023-06-16 08:49:57 +0530

    Update bubble_sort.py

--- 2023_leetcode_python/bubble_sort.py
+++ 2023_leetcode_python/bubble_sort.py
@@ -1,9 +1,12 @@
 def bubblesort(arry):
+    
     for i in range(0, len(arry)-1):
+        print(arry[i])
+        print(arry[i + 1])
         if arry[i] > arry[i+1]:
             arry[i] , arry[i+1] = arry[i+1], arry[i]
         
-        
+    return arry    
                         
 arry = [2,8,4,1,-6]
 print(bubblesort(arry))
