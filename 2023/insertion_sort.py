2e03746b9d37061fb489331f761c576dcf2b1bf0
Hemanth Kumar <digitalhemanth@gmail.com>
2023-07-13 09:08:30 +0530

    Update insertion_sort.py

--- 2023/insertion_sort.py
+++ 2023/insertion_sort.py
@@ -1,7 +1,17 @@
 #insertion sort
 
-def insertion_sort(arrays):
-    
-
-array = [2, 4, 5, 1, 6, 3]
-print(insertion_sort(array))
+def insertion_sort(arr):
+    for i in range(1,len(arr)):
+        print(arr)
+        print('working on ', arr[i])
+        ele = arr[i]
+        j = i-1
+        while j >=0 and arr[j] > ele:
+           arr[j+1] = arr[j]
+           j -= 1
+        arr[j+1] = ele 
+        print(f'{arr} \n')
+    return arr       
+ 
+arr = [5, 2, 4, 6, 1, 3]
+print('\n',insertion_sort(arr))
